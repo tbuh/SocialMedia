f1ef51d6975af9aac5dee6ecc43beb2d312ecdc9
Taras B <Taras.Buha@gmail.com>
2018-06-20 11:29:53 +0300

    Added SignalR

---
+++ SocialMedia/Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public Task Send(string data)
+        {
+            return Clients.All.SendAsync("Send", "Send Test Done...");
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+    }
+}
